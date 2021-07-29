5ba18d147642893eded82db0037158ad8836f028
vKutik <84785564+vKutik@users.noreply.github.com>
2021-07-29 20:58:13 +0300

    Ready to deploy

--- src/main/java/taxi/util/ConnectionUtil.java
+++ src/main/java/taxi/util/ConnectionUtil.java
@@ -9,7 +9,7 @@ import org.apache.logging.log4j.Logger;
 import taxi.dao.impl.CarDaoImpl;
 
 public class ConnectionUtil {
-    public static final String URL = "jdbc:mysql://remotemysql.com:3306/v4bPdUzyQL?serverTimezone=UTC";
+    public static final String URL = "jdbc:mysql://remotemysql.com:3306/v4bPdUzyQL";
     public static final String USERNAME = "v4bPdUzyQL";
     public static final String PASSWORD = "nTLMAN7FKJ";
     public static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
